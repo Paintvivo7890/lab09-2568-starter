a11386a84f07d5c359356764076f03972afd4dfa
Ppaint <narudee_sornsawat@cmu.ac.th>
2025-08-06 14:48:03 +0700

    up

--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,17 @@
+import {useState} from "react";
+
 export default function HelloMyNamePage() {
+  const [name,setName] = useState("");
+  const [result,setResult] = useState("");
+  const nameInput = (event:any)=>{
+    setName(event.target.value);
+  };
+  
+  const greetBtnOnclicke = () => {
+    setResult(`Hi kub ${name}`);
+  };
+  
+  
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +19,12 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        onChange={nameInput}
+        value={name}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary" onClick={greetBtnOnclicke}>Greet Me</button>
       {/* Result Text */}
-      <p></p>
+      <p>{result}</p>
     </div>
   );
 }
